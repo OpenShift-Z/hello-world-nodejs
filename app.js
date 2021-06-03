79bb9c40df0f99d26489b9522bc84f296b481399
Elton de Souza <48670536+e-desouza@users.noreply.github.com>
2021-06-03 13:35:41 -0400

    changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
     platform: process.platform,
     bit : process.arch,
     runtime : process.versions.node,
-    endian : os.endianness
+    endian : 🛢
   });
 });
 
