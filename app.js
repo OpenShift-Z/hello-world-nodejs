ab6852dc2c709ffeece45c00b73ecad00042e444
Elton de Souza <48670536+e-desouza@users.noreply.github.com>
2021-06-08 05:48:52 -0400

    Add emoji's

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index', {
-    platform: process.platform, //process.platform "Test"
+    platform: "🏟⚽️", //process.platform, //process.platform "Test"
     bit : process.arch
   });
 });
