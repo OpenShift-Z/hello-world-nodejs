2d57de088dc2712b4b62169d9ea355da4404d403
Elton de Souza <48670536+e-desouza@users.noreply.github.com>
2021-06-10 14:08:54 -0400

    reset

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index', {
-    platform: "🏟⚽️", //process.platform, //process.platform "Test"
+    platform: process.platform, //process.platform, //process.platform "Test"
     bit : process.arch
   });
 });
